dcae36f30e21908fbbe01c49990118741e7af2ab
developerthierry <developerthierry@gmail.com>
2025-11-22 19:32:33 +0000

    Language Chips

--- App.jsx
+++ App.jsx
@@ -1,17 +1,5 @@
 import React from "react";
-
-/**
- * Project planning:
- *
- * Questions to ask yourself before writing any code:
- *
- * - What are the main containers of elements I need
- *   in this app?
- * - What values will need to be saved in state vs.
- *   what values can be derived from the state?
- * - How will the user interact with the app? What
- *   events do I need to handle?
- */
+import { languages } from "./languages";
 
 export default function AssemblyEndgame() {
   return (
@@ -26,6 +14,19 @@ export default function AssemblyEndgame() {
       <section className="game-status">
         <h2>You win!</h2>
         <p>Well done! 🎉</p>
+      </section>
+      <section className="language-chips">
+        {languages.map((lang) => {
+          const styles = {
+            backgroundColor: lang.backgroundColor,
+            color: lang.color,
+          };
+          return (
+            <span className="chip" style={styles} key={lang.name}>
+              {lang.name}
+            </span>
+          );
+        })}
       </section>
     </main>
   );
